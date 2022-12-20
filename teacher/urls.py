64dba9052d93a37c0da563cdc9b537ccf247be62
anandkumar01 <vishwakarmaanand858@gmail.com>
2022-12-21 04:01:41 +0530

    css update

--- teacher/urls.py
+++ teacher/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path("subject/",views.subject,name="subject"),
 	path("coordinator/login/",views.coordinatorlogin,name="coordinator"),
     path('about/',views.about,name="about"),
-    path('mark_attendance/',views.mark,name="mark"),
     path('your-timetable/',views.teachertimetable,name="teacher_timetable"),
     path('forget/',views.forget,name='forget'),
     path('mail/',views.forgot_mail),
